4263813bb859fdacd29a23c58900ff438a8a462e
George E. Correa <george.e.correa@gmail.com>
2019-09-26 20:11:05 -0400

    Set business fields using data from business class

--- ios-code-challenge/Cells/NXTBusinessTableViewCell+YLPBusiness.m
+++ ios-code-challenge/Cells/NXTBusinessTableViewCell+YLPBusiness.m
@@ -15,6 +15,27 @@
 {
     // Business Name
     self.nameLabel.text = business.name;
+    // Categories
+    self.categoryLabel.text = business.categories;
+    // Rating
+    self.ratingLabel.text = [NSString stringWithFormat:@"Rating: %0.1f", business.rating];
+    // Number of reviews
+    self.reviewCountLabel.text = [NSString stringWithFormat:@"Number of reviews: %lu", business.reviewCount];
+    // Distance
+    self.distanceLabel.text = [NSString stringWithFormat:@"Distance: %0.2f", business.distance];
+    // Thumbnail - Image View
+    self.businessImageView.clipsToBounds = YES;
+    self.businessImageView.contentMode = UIViewContentModeCenter;
+    self.businessImageView.contentMode = UIViewContentModeScaleAspectFit;
+    // Set image
+    dispatch_async(dispatch_get_global_queue(0,0), ^{
+        NSData * data = [[NSData alloc] initWithContentsOfURL: [NSURL URLWithString: business.imageUrlString]];
+        if ( data == nil )
+            return;
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.businessImageView.image = [UIImage imageWithData: data];
+        });
+    });
 }
 
 #pragma mark - NXTBindingDataForObjectDelegate
