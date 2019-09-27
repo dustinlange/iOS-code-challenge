3cf2b809529ddbdffc0e3b63d93c36ecc61a7201
George E. Correa <george.e.correa@gmail.com>
2019-09-26 20:07:32 -0400

    Add required fields to business cell in Search tab

--- ios-code-challenge/Cells/NXTBusinessTableViewCell.h
+++ ios-code-challenge/Cells/NXTBusinessTableViewCell.h
@@ -11,5 +11,10 @@
 @interface NXTBusinessTableViewCell : UITableViewCell
 
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *categoryLabel;
+@property (weak, nonatomic) IBOutlet UILabel *ratingLabel;
+@property (weak, nonatomic) IBOutlet UILabel *reviewCountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *distanceLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *businessImageView;
 
 @end
--- ios-code-challenge/YelpAPI/YLPBusiness+CellForObject.m
+++ ios-code-challenge/YelpAPI/YLPBusiness+CellForObject.m
@@ -29,7 +29,7 @@ NSString *const kNXTBusinessTableViewCellIdentifier = @"NXTBusinessTableViewCell
 
 - (CGFloat)estimatedCellHeightForObjectForTableView:(UITableView *)tableView
 {
-    return 55.0f;
+    return 150.0f;
 }
 
 @end
